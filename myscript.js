6c5fdaca130fd0f8b7c8a81d41871c307323c7ab
Juanruz <juan.rgarcia@hotmail.com>
2024-07-23 20:28:05 -0300

    Add getHumanChoice() to get the user choice

--- myscript.js
+++ myscript.js
@@ -4,3 +4,11 @@ function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     return computerChoice;
 }
+
+function getHumanChoice(){
+    let humanChoice = Number(prompt("1: Pedra \n2: Papel \n3: Tesoura"));
+    console.log(humanChoice);
+    return humanChoice;
+}
+
+getHumanChoice();
